894c47896e34311a76e2f2b497f8d79e067e1804
u_vovchika <u-vovchika116@yandex.ru>
2024-11-18 21:12:22 +0300

    DZ_Nizam_14_2

--- src/DZ_Nizam_14.java
+++ src/DZ_Nizam_14.java
@@ -2,5 +2,7 @@ public class DZ_Nizam_14 {
     public static void main(String[] args) {
         System.out.println("Доброго времени суток Елена");
         System.out.println("Домашнее задание !!!");
+        System.out.println("Всех благ !!!");
+
     }
 }
