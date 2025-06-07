ebea3e40ce67bb672d28e75a9abbbffec02f869d
u_vovchika <u-vovchika116@yandex.ru>
2025-06-07 23:56:31 +0300

    Ext_SD_Card__NZ

--- FileManagerProjectAplication/app/src/main/java/com/example/filemanagerprojectaplication/MainActivity.java
+++ FileManagerProjectAplication/app/src/main/java/com/example/filemanagerprojectaplication/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.filemanagerprojectaplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.filemanagerprojectaplication.fragments.CardFragment;
+import com.example.filemanagerprojectaplication.fragments.InternalFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        /// Эти две строки должны быть обязательнгы, когда работаем с ToolBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        /// /////////////////////////////////////////////////////////////////
+
+        /// доступы ко всем элементам которые нам нужны
+        NavigationView navigationView = findViewById(R.id.nav_view); // боковая палитра навигации
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout); // доступ к основному контейнеру drawer_layout
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string
+                .open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        //toggle.syncState(); //чтобы иконка появилась.
+
+        //replaceFragment(new InternalFragment());
+        replaceFragment(new CardFragment());
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_internal) {
+                    replaceFragment(new InternalFragment());
+                } else if (item.getItemId() == R.id.nav_card) {
+                    replaceFragment(new CardFragment());
+                } else if (item.getItemId() == R.id.nav_about) {
+                    Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
         });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ FileManagerProjectAplication/app/src/main/java/com/example/filemanagerprojectaplication/fragments/InternalFragment.java
@@ -0,0 +1,128 @@
+package com.example.filemanagerprojectaplication.fragments;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.filemanagerprojectaplication.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InternalFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<File> fileList;
+    private ImageView ingBack;
+    TextView tvPathHolder;
+    File storage;
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_internal, container, false);
+
+        ingBack = view.findViewById(R.id.img_back);
+        tvPathHolder = view.findViewById(R.id.tv_path_holder);
+
+        // Получаем доступ к внутренней SD-карте
+        String internalStorage = System.getenv("EXTERNAL_STORAGE");
+        storage = new File(internalStorage);
+
+        tvPathHolder.setText(storage.getAbsolutePath());
+        runtimePermission();
+
+        return view;
+    }
+
+    private void runtimePermission() {
+        //разрешение до Android 10 (API 29)
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission
+                    .READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission
+                        .READ_EXTERNAL_STORAGE}, 100);
+            }
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission
+                    .READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                displayFiles();
+            }
+        }
+        //разрешение для андроид 11 ( API 30) и выше
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                try {
+                    Uri uri = Uri.fromParts("package", getActivity().getPackageName(),
+                            null);
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                    intent.setData(uri);
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setData(Uri.parse(String.format("package:%s", getActivity().getPackageName())));
+                    getActivity().startActivityIfNeeded(intent, 101);
+
+                } catch (Exception e) {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                    getActivity().startActivityIfNeeded(intent, 101);
+                }
+            }
+            if (!Environment.isExternalStorageManager()) {
+                displayFiles();
+            }
+        }
+    }
+
+    public ArrayList<File> findFiles(File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for (File singleFile : files) {
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                arrayList.add(singleFile);
+            }
+        }
+
+        for (File singleFile : files) {
+            if (singleFile.getName().toLowerCase().endsWith(".jpeg") ||
+                    singleFile.getName().toLowerCase().endsWith(".jpg") ||
+                    singleFile.getName().toLowerCase().endsWith(".png") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp3") ||
+                    singleFile.getName().toLowerCase().endsWith(".wav") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp4") ||
+                    singleFile.getName().toLowerCase().endsWith(".pdf") ||
+                    singleFile.getName().toLowerCase().endsWith(".doc") ||
+                    singleFile.getName().toLowerCase().endsWith(".apk")) {
+                arrayList.add(singleFile);
+            }
+        }
+        return arrayList;
+    }
+
+    private void displayFiles() {
+        recyclerView = view.findViewById(R.id.recycler_internal);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        fileList = new ArrayList<>();
+        fileList.addAll(findFiles(storage));
+    }
+}
